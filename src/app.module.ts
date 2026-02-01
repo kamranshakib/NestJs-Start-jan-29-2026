7a8ffdd764ba39f46280e0ad5da755c292a84850
Kamran Shakib <kamranshakib371@gmail.com>
2026-02-01 09:54:17 -0800

    apply HTTP logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { HelloModule } from './hello/hello.module';
+import { LoggerMiddleware } from './utils/logger.middleware';
 
 @Module({
   controllers: [],
   providers: [],
   imports: [HelloModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/hello/hello.service.ts
+++ src/hello/hello.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class HelloService {
+  logger: Logger;
+  constructor() {
+    this.logger = new Logger('CUSTOM LOGGER');
+  }
   getHello(): string {
+    this.logger.log('test');
     const name = 'Kamran Shakib';
     return `Hello ${name} This is your first route in NestJS 😎`;
   }
