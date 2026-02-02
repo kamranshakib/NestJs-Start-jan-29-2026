2ef029b478c405066c28db60950f527be729406f
Kamran Shakib <kamranshakib371@gmail.com>
2026-02-02 08:29:00 -0800

    create a simple dto file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dtos/signup.dtos';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   //TODO: POST sign up
+  @Post('signup') //auth/signup
+  async signup(@Body() signUpDto: SignUpDto) {}
 
   //TODO: POST login
 
