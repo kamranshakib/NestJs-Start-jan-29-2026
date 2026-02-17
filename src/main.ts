70fbedb6c271382fb6d3861b0873d7e1bf19c1d5
Kamran Shakib <kamranshakib371@gmail.com>
2026-02-17 09:59:04 +0430

    use multer for upload file

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const config = new DocumentBuilder().setTitle('nest Swagger').build();
+  const config = new DocumentBuilder().setTitle('nest app').build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/documentation', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
+
       transformOptions: { enableImplicitConversion: true },
     }),
   );
